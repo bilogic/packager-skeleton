d740816cfe9823fd24311fe10442bd88a4569548
bilogic <946010+bilogic@users.noreply.github.com>
2023-07-28 23:14:18 +0800

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,8 +4,11 @@ namespace :uc:vendor\:uc:package\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends :uc:vendor\:uc:package\Tests\Support\TestCaseWithLaravel;
 {
+    use \Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+    use \Illuminate\Foundation\Testing\DatabaseMigrations;
+
     public function test_true_is_true(): void
     {
         self::assertTrue(true);

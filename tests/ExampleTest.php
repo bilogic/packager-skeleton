0567c12137891ffbb9f4e2f117f66b66b744ce68
bilogic <946010+bilogic@users.noreply.github.com>
2023-12-03 13:48:46 +0800

    Update ExampleTest.php
    remove clash

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,10 +3,7 @@
 namespace :uc:vendor\:uc:package\Tests;
 
 class ExampleTest extends TestbenchTestCase
-{
-    use \Illuminate\Foundation\Testing\LazilyRefreshDatabase;
-    use \Illuminate\Foundation\Testing\DatabaseMigrations;
-
+{   
     public function test_true_is_true(): void
     {
         self::assertTrue(true);
